5176b7ca27d687c925ed6735f9f6e08e13486fd7
Ankit Bourasu <ankitbourasi0@gmail.com>
2026-02-20 23:11:07 +0530

    bubble tea setup

---
+++ Readme.md
@@ -0,0 +1,19 @@
+## Install Bubble Tea 
+1. import in main.go
+```go
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+```
+2. Run command in terminal: `go mod tidy`  
+Above command download this package.
+
+
+## Architecture of bubble tea
+Bubble tea work on ELM architecture, it consists of 3 phase as follows
+1. **Init** - a function that returns an initial command for the application to run.
+2. **Update** - a function that handles incoming events and updates the model accordingly.
+3. **View** -  a function that renders the UI based on the data in the model.
+
+
+![img.png](img.png)
--- main.go
+++ main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	msg string
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+// Update method usually receive a message type, but we can use type assertion also
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	//Is it a key press?
+	case tea.KeyMsg:
+
+		//Great, what was the actual press?
+		switch msg.String() {
+		//Exit the application when these keys pressed.
+		case "ctrl+c", "q":
+			fmt.Println("\n\nuser pressed", msg.String(), "Application Exit")
+			return m, tea.Quit
+
+		}
+	}
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+func (m model) View() string {
+	return m.msg
+}
+
+func initializeMode() model {
+	return model{
+		msg: "Initializing...",
+	}
+}
+
+func main() {
+	p := tea.NewProgram(initializeMode())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Ahh! there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
